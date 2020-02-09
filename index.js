19e86f40b5532e9f815fcdaf9c36c3887c633685
Jordan Pio <czpjunior@gmail.com>
2020-02-10 05:42:04 +1100

    index change

--- index.js
+++ index.js
@@ -37,48 +37,47 @@
 //   await browser.close();
 // })();
 
-
 const puppeteer = require("puppeteer");
 
 (async function main() {
   try {
     // to get started we need a browser and a page object
 
-    const browser = await puppeteer.launch({headless: false})
-    const page = await browser.newPage()
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
 
     // setup different user agent as this wont be detected as a robot
     // the one that puppeter gives as a default is detectable
-    page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.0 Safari/537.36')
+    page.setUserAgent(
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.0 Safari/537.36"
+    );
     // we need to start a goto page
-    await page.goto('https://experts.shopify.com/')
+    await page.goto("https://experts.shopify.com/");
     // you need to tell the next thing to do
     // in this case we will be waiting for section to appear
-    await page.waitForSelector('._1rA3U')
-    console.log('its showing')
+    await page.waitForSelector("._1rA3U");
+    console.log("its showing");
 
-    const sections = await page.$$('._17BF0')
+    const sections = await page.$$("._17BF0");
     // console.log(sections.length)
     // never use forEach
 
     for (const section of sections) {
-      const button = await section.$('span._1rA3U')
-      button.click()
-      await page.waitForSelector('.Zk7Cl')
-      const divs = await page.$$('div._3iz-t')
+      const button = await section.$("span._1rA3U");
+      button.click();
+      await page.waitForSelector(".Zk7Cl");
+      const divs = await page.$$("div._3iz-t");
 
       // loop over each div on the page
 
       for (const div of divs) {
-        const name = await div.$eval('h2', h2 => h2.innerText)
-        console.log('name', name)
+        const name = await div.$eval("h2", h2 => h2.innerText);
+        console.log("name", name);
       }
-
     }
 
-
-    
+    // test
   } catch (e) {
-    console.log("error", e)
+    console.log("error", e);
   }
 })();
